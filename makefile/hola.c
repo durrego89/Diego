6899e4a7e5ede63a5b990d02b0093d960ae93948
Diego Barbosa <diegobarbosa@Diegos-MacBook-Pro.local>
2016-02-13 12:03:20 -0500

    primer archivo en C

---
+++ makefile/hola.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  hola.c
+//
+//  Created by Diego Barbosa on 2/13/16.
+//  Copyright © 2016 Diego Barbosa. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main() {
+    // insert code here...
+    printf("Hola mundo!\n");
+    return 0;
+}
