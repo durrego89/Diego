e407372f606e618011117650b4fe97ac8a8c56a6
Diego <da.barbosa10@uniandes.edu.co>
2016-02-13 12:21:09 -0500

    Segundo C

---
+++ makefile/numeros.c
@@ -0,0 +1,21 @@
+//
+//  main.c
+//  numeros.c
+//
+//  Created by Diego Barbosa on 2/13/16.
+//  Copyright © 2016 Diego Barbosa. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main() {
+    int i;
+    int m;
+    for (i=0; i<10; i++) {
+        m=i*2;
+        printf("%d \t %d\n", i,m);
+    }
+    
+    printf("Hello, World!\n");
+    return 0;
+}
